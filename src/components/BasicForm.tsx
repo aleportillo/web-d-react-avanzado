a4b913f941d4af0fa10f42982373b2742b95514f
Freida Portillo <alejandrabypoca@gmail.com>
2025-07-13 00:25:09 -0600

    feat: add form validations

--- src/components/BasicForm.tsx
+++ src/components/BasicForm.tsx
@@ -1,7 +1,19 @@
+import { yupResolver } from '@hookform/resolvers/yup'
 import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+
+const schema = yup.object({
+  username: yup.string().required('El nombre es obligatorio'),
+  password: yup.string().min(6, 'La contraseña debe tener mínimo 6 caracteres.').required('La contraseña es obligatoria'),
+  confirmPassword: yup.string().oneOf([yup.ref('password')], 'Las contraseñas no coinciden').required('Confirma tu contraseña')
+})
 
 export const BasicForm = () => {
-  const { register, handleSubmit } = useForm()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid }
+  } = useForm({ resolver: yupResolver(schema) })
 
   const onSubmit = (data:unknown) => {
     console.log(data)
@@ -9,10 +21,18 @@ export const BasicForm = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input type='type' {...register('username')} placeholder='Usuario' />
-      <input type='password' {...register('password')} placeholder='Password' />
-      <input type='password' {...register('confirmPassword')} placeholder='Confirm' />
-      <button type='submit'> Enviar </button>
+      <input type='type' {...register('username')} placeholder='Usuario' className='w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400' />
+      {errors.username && <p>{errors.username.message}</p>}
+      <br />
+      <input type='password' {...register('password')} placeholder='Password' className='w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400' />
+      {errors.password && <p>{errors.password.message}</p>}
+      <br />
+
+      <input type='password' {...register('confirmPassword')} placeholder='Confirm' className='w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400' />
+      {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
+      <br />
+
+      <button type='submit' disabled={!isValid} className={`w-full py-2 rounded transition cursor-pointer ${isValid ? 'bg-blue-600 text-white hover:bg-blue-700' : 'bg-gray-300 text-gray-500 cursor-not-allowed'}`}> Enviar </button>
     </form>
   )
 }
