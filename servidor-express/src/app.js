3181a56a23dd59f673d84e1ceb84ad0816febb39
Freida Portillo <alejandrabypoca@gmail.com>
2025-07-27 01:57:19 -0600

    feat: express server

---
+++ servidor-express/src/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express()
+
+const PORT = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hola mundo')
+})
+
+app.get('/segundaruta', (req, res) => {
+  res.send('Hola desde la segunda ruta')
+})
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`)
+})
