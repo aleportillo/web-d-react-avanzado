614dfc7551cec6024bd03e2e9378bafc7b468314
Freida Portillo <alejandrabypoca@gmail.com>
2025-07-29 23:52:24 -0600

    feat: test express endpoints

--- servidor-express/src/app.js
+++ servidor-express/src/app.js
@@ -1,8 +1,12 @@
+require('dotenv').config()
+
 const express = require('express')
+
+const { infoPeliculas } = require('./peliculas')
 
 const app = express()
 
-const PORT = 3000
+const PORT = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('Hola mundo')
@@ -10,6 +14,48 @@ app.get('/', (req, res) => {
 
 app.get('/segundaruta', (req, res) => {
   res.send('Hola desde la segunda ruta')
+})
+
+app.get('/api/peliculas', (req, res) => {
+  res.send(infoPeliculas)
+})
+
+app.get('/api/peliculas/accion/:titulo/:year', (req, res) => {
+  const { year, titulo } = req.params
+
+  const response = infoPeliculas.accion.filter(p => p.titulo === titulo && p.year === Number(year))
+
+  if (!response.length) {
+    res.status(400).send(`No se encuentran resultados con: ${titulo}, ${year}`)
+    return
+  }
+
+  res.send(response)
+})
+
+app.get('/api/peliculas/comedia/:pais', (req, res) => {
+  const { ordenar } = req.query
+  const { pais } = req.params
+
+  const resultados = infoPeliculas.comedia.filter(p => p.pais === pais)
+
+  if (ordenar === 'year') {
+    res.send(resultados.sort((a, b) => b.year - a.year))
+  }
+
+  res.send(resultados)
+})
+
+app.use(express.json())
+app.post('/api/peliculas', (req, res) => {
+  const nuevaPelicula = req.body
+
+  console.log(nuevaPelicula)
+
+  res.status(200).send({
+    mensaje: 'La pelicula se recibio con exito',
+    datos: nuevaPelicula
+  })
 })
 
 app.listen(PORT, () => {
---
+++ servidor-express/src/peliculas.js
@@ -0,0 +1,44 @@
+const infoPeliculas = {
+  accion: [
+    {
+      id: 1,
+      titulo: 'Mision imposible',
+      year: 2005,
+      pais: 'usa'
+    },
+    {
+      id: 2,
+      titulo: 'Rapidos y furiosos',
+      year: 2009,
+      pais: 'usa'
+    },
+    {
+      id: 3,
+      titulo: 'John Wick',
+      year: 2014,
+      pais: 'usa'
+    }
+  ],
+  comedia: [
+    {
+      id: 1,
+      titulo: 'Superbad',
+      year: 2007,
+      pais: 'usa'
+    },
+    {
+      id: 2,
+      titulo: 'La máscara',
+      year: 1994,
+      pais: 'usa'
+    },
+    {
+      id: 3,
+      titulo: '¿Qué pasó ayer?',
+      year: 2009,
+      pais: 'usa'
+    }
+  ]
+}
+
+module.exports.infoPeliculas = infoPeliculas
